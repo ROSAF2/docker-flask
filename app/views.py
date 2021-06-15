afdfe3bdce27fb52edf069e4083946aa2cc778eb
Francisco Rosas <rosaf2@student.op.ac.nz>
2021-06-15 21:07:51 +0000

    Redis code added to run.py

--- app/views.py
+++ app/views.py
@@ -1,5 +1,22 @@
 from app import app
 import os
+import time
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 @app.route("/")
 def index():
@@ -10,4 +27,5 @@ def index():
     if app_name:
         return f"Hello from {app_name} running in a Docker container behind Nginx!"
 
-    return "Hello from Flask"
+    count = get_hit_count()
+    return 'Hello World! from Flask I have been seen {} times.\n'.format(count)
